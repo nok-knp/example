7737fd658bdc07c1ef35c6566993ca18b8762cf3
My knp <kanokphonephilavong@gmail.com>
2025-06-14 21:32:36 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql2");
+const cors = require("cors")
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET || "key";
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+app.use(cors())
+const port = 3000;
+
+app.use(bodyParser.json()); // Parse JSON requests
+
+// Database connection
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "nn",
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log("Database connected!");
+});
+
+//post for register
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  db.query("SELECT * FROM users WHERE username = ?", [username], async (err, result) => {
+    if (result.length > 0) {
+      return res.status(400).json({ message: "Username already taken" });
+    }
+
+    const hashed = await bcrypt.hash(password, 10);
+    db.query("INSERT INTO users (username, password) VALUES (?, ?)", [username, hashed], (err, result) => {
+      if (err) return res.status(500).json({ error: err });
+      res.status(201).json({ id: result.insertId, username });
+    });
+  });
+});
+
+//post for login
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  db.query("SELECT * FROM users WHERE username = ?", [username], async (err, results) => {
+    if (err) return res.status(500).json({ error: err });
+    if (results.length === 0) return res.status(401).json({ message: "User not found" });
+
+    const user = results[0];
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(401).json({ message: "Invalid password" });
+
+    const token = jwt.sign({ id: user.id, username: user.username }, JWT_SECRET, { expiresIn: "1h" });
+    res.json({ token });
+  });
+});
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) return res.status(401).json({ message: "Token required" });
+
+  jwt.verify(token, JWT_SECRET, (err, user) => {
+    if (err) return res.status(403).json({ message: "Invalid token" });
+    req.user = user;
+    next();
+  });
+};
+
+app.get("/home", authenticateToken, (req, res) => {
+  res.json({ message: `Welcome, ${req.user.username}!` });
+});
+
+//for error 404
+app.use((req, res) => {
+  res.status(404).json({ message: "Not Found" });
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+}
+);
+
+
+
