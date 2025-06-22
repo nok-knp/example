2baba2b67f4e6110190a50630dc7de4d8c26f28c
nok-knp <kanokphonephilavong@gmail.com>
2025-06-22 19:42:24 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mysql = require("mysql2");
+const cors = require("cors");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = process.env.JWT_SECRET || "key";
